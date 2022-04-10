aa052403abfadeca1246979a0d1ff41e3d7377e8
Edgar Soares <edemarinho@outlook.com>
2022-04-10 20:11:30 -0300

    DEV: Criação do server com o express

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import "dotenv/config";
+import http from "http";
+import { app } from "./app";
+
+const normalizePort = (val: string) => {
+	const port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		return val;
+	}
+	if (port >= 0) {
+		return port;
+	}
+	return false;
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const errorHandler = (error: { syscall: string; code: any }) => {
+	if (error.syscall !== "listen") {
+		throw error;
+	}
+	const address = server.address();
+	const bind =
+		typeof address === "string" ? "pipe " + address : "port: " + port;
+	switch (error.code) {
+		case "EACCES":
+			console.error(bind + " requires elevated privileges.");
+			process.exit(1);
+			break;
+		case "EADDRINUSE":
+			console.error(bind + " is already in use.");
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+};
+
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+	console.log("Listening on http://localhost:" + port);
+});
+
+server.listen(port);
